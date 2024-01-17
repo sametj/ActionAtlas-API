64df59cae701de2a0d95e33186e769f66b11be7a
sametj <mosesjames2000@gmail.com>
2024-01-17 11:55:44 -0500

    testing

--- api/app.js
+++ api/app.js
@@ -17,7 +17,6 @@ connectToDB();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
