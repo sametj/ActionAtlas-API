d069d8a62e0146c22f73be470cccbd83845178de
sametj <mosesjames2000@gmail.com>
2024-01-17 21:34:39 -0500

    testing

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
   next();
 });
 
