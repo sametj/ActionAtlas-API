01191db9192e7253b04e7df361233b21885ccbc4
sametj <mosesjames2000@gmail.com>
2024-01-17 23:05:00 -0500

    added header

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ const userSchema = require("./api/models/userSchema");
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
+
+app.use(express.json());
 
 require("dotenv").config();
 
